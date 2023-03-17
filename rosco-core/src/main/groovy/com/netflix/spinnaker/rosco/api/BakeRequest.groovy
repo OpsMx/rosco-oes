45c8f0b975ef236c92e708be1f4a3ba594245646
SheetalAtre <sheetal.atre@opsmx.io>
2023-03-17 14:45:25 +0530

    changes given by sanjeev

--- rosco-core/src/main/groovy/com/netflix/spinnaker/rosco/api/BakeRequest.groovy
+++ rosco-core/src/main/groovy/com/netflix/spinnaker/rosco/api/BakeRequest.groovy
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 Netflix, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.netflix.spinnaker.rosco.api
 
@@ -27,11 +12,6 @@ import groovy.transform.CompileStatic
 import groovy.transform.Immutable
 import io.swagger.annotations.ApiModelProperty
 
-/**
- * A request to bake a new machine image.
- *
- * @see BakeryController#createBake(String, BakeRequest, String)
- */
 @Immutable(copyWith = true)
 @CompileStatic
 class BakeRequest {
